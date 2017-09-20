ca2a10b9899600cdc1fee5148872567dbe938b44
Howard Feng <feng-hua-yu@hotmail.com>
2017-09-20 19:41:06 +0800

    asda

--- class/DataBase.class.php
+++ class/DataBase.class.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST','127.0.0.1');
 define('DB_USER','root');
-define('DB_PWD','***REMOVED***');
+//define('DB_PWD','***REMOVED***');
 define('DB_PWD','root');
 define('DB_NAME','junxun');
 
